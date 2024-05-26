8922971726315f7b3b835630bb59d7e773220911
Unique Neupane <133880428+Unique982@users.noreply.github.com>
2024-05-26 18:27:52 +0545

    Update add_course.php
    update code

--- add_course.php
+++ add_course.php
@@ -1,5 +1,11 @@
 <?php
+session_start();
+
 include './database/config.php';
+if(!isset($_SESSION['username'])){
+ header("Location:login.php");
+}
+
 if(isset($_POST['add'])){
     $course_code = mysqli_real_escape_string($conn,$_POST['course_code']);
     $course_name = mysqli_real_escape_string($conn,$_POST['course_name']);
@@ -36,22 +42,31 @@ if(isset($_POST['add'])){
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-      <h1>Course</h1>
-      <form action="" method="POST">
-      <div class="input-box">
-            <label>Course Code</label>
-            <input type="text" name="course_code">
+     <div class="container">
+      <div class="row">
+       <div class="col-md-5 mx-auto">
+        <div class="card  bg-light">
+          <div class="card-header bg-secondary text-light text-center">
+            <b>Add Course</b>
+          </div>
+          <div class="card-body">
+            <form action="" method="POST">
+              <div class="form-group">
+              <label>Course Code</label>
+            <input type="text" name="course_code" class="form-control">
+              </div>
+              <div class="form-group">
+              <label>Course</label>
+            <input type="text" name="course_name" class="form-control">
+              </div>
+              <div class="form-group">
+                <button type="submit" name="add" class="btn btn-outline-warning text-dark">ADD</button>
+
+              </div>
+            </form>
+          </div>
         </div>
-        <div class="input-box">
-            <label>Course</label>
-            <input type="text" name="course_name">
-        </div>
-        <input type="submit" name="add" value="ADD Course">
-      </form>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+       </div>
+      </div>
+     </div>
 </html>
