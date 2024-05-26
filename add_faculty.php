f8af16a249cbff7fbb1d2177c682d18bc07e9334
Unique Neupane <133880428+Unique982@users.noreply.github.com>
2024-05-26 18:29:04 +0545

    Update add_faculty.php
    update code

--- add_faculty.php
+++ add_faculty.php
@@ -1,5 +1,9 @@
 <?php 
+session_start();
  include './database/config.php';
+ if(!isset($_SESSION['username'])){
+  header("Location:login.php");
+ }
  if(isset($_POST['faculty'])){
     $faculty_name = mysqli_real_escape_string($conn,$_POST['faculty_name']);
     $designation = mysqli_real_escape_string($conn,$_POST['designation']);
@@ -20,7 +24,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Title</title>
+    <title>Faculty Add</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -29,18 +33,32 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-      <h1>Faculty Add</h1>
-      <form action="" Method="POST" enctype="multipart/form-data">
-        <div class="input-box">
-            <label for="">Faculty Name:</label>
-            <input type="text" name="faculty_name">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 mx-auto">
+          <div class="card bg-light">
+            <div class="card-header bg-success text-light text-center">
+              <b>Add Faculty</b>
+            </div>
+            <div class="card-body">
+              <form action="" method="POST">
+                <div class="form-group">
+                <label for="">Faculty Name:</label>
+            <input type="text" name="faculty_name" class="form-control">
+                </div>
+                <div class="form-group">
+                <label for="">Designation:</label>
+            <input type="text" name="designation" class="form-control">
+                </div>
+                <div class="form-group">
+                  <button type="submit" name="faculty" class="btn btn-outline-primary">ADD Faculty</button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-        <div class="input-box">
-            <label for="">Designation:</label>
-            <input type="text" name="designation">
-        </div>
-        <input type="submit" name="faculty" value="Add">
-      </form>
+      </div>
+    </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -48,3 +66,5 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </body>
 </html>
+
+ 
