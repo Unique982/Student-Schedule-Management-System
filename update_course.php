ce7e31a45de445b44fda2eb9cf9ee430cf964225
Unique Neupane <133880428+Unique982@users.noreply.github.com>
2024-05-26 18:28:34 +0545

    Update update_course.php
    update course code

--- update_course.php
+++ update_course.php
@@ -1,13 +1,18 @@
 <?php 
 include './database/config.php';
+session_start();
+if(!isset($_SESSION['username'])){
+ header("Location:login.php");
+}
 if(isset($_POST['edit'])){
     $coid =$_POST['coid'];
  $course_code = mysqli_real_escape_string($conn,$_POST['course_code']);
  $course_name = mysqli_real_escape_string($conn,$_POST['course_name']);
  $sql = "UPDATE course SET course_code='$course_code', course_name='$course_name' WHERE course_id = '$coid'";
  if(mysqli_query($conn,$sql)){
+  echo "update data successfully! ";
     header("location:course.php");
-    echo "update data successfully! ";
+   exit();
  }
  else{
     echo "Update data failed";
@@ -29,27 +34,46 @@ if(isset($_POST['edit'])){
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-    <?php 
+  <div class="container">
+      <div class="row">
+   
+        <div class="col-md-8 mx-auto">
+          <div class="card bg-light">
+            <div class="card-header bg-success text-dark text-center">
+              <b>Update Course</b>
+            </div>
+               <?php 
     $coid =$_GET['coid'];
      $sqli = "SELECT * FROM course WHERE course_id='$coid'";
    $resuly = mysqli_query($conn,$sqli) or die("Query Failed");
-   if(mysqli_num_rows($resuly)){
+   if(mysqli_num_rows($resuly)>0){
     while($row = mysqli_fetch_array($resuly)){
     ?>
-      <h1>Course</h1>
-      <form action="" method="POST">
-        <input type="hidden" name="coid" value="<?php echo $row['course_id'] ?>">
-      <div class="input-box">
-            <label>Course Code</label>
-            <input type="text" name="course_code" value="<?php echo $row['course_code'] ?>">
+            <div class="card-body">
+              <form action="" method="POST" enctype="multipart/form-data">
+              <input type="hidden" name="coid" value="<?php echo $row['course_id'] ?>">
+                <div class="form-group">
+                <label>Course Code</label>
+            <input type="text" name="course_code" value="<?php echo $row['course_code'] ?>" class="form-control">
+                </div>
+                <div class="form-group">
+                <label>Course</label>
+            <input type="text" name="course_name" value="<?php echo $row['course_name']?>" class="form-control">
+                </div>
+                <div class="form-group">
+                  <button type="submit" name="edit" class="btn btn-primary">Update</button>
+                </div>
+              </form>
+              
+            </div>
+            <?php  } }?>
+               
+          </div>
         </div>
-        <div class="input-box">
-            <label>Course</label>
-            <input type="text" name="course_name" value="<?php echo $row['course_name']?>">
-        </div>
-        <input type="submit" name="edit" value="ADD Course">
-      </form>
-      <?php }}  ?>
+      </div>
+    </div>
+
+ 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
