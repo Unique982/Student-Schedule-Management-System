49f554aa2492d0ca458538050040ce22818ece37
Unique Neupane <133880428+Unique982@users.noreply.github.com>
2024-05-26 18:29:37 +0545

    Update update_faculty.php
    update code

--- update_faculty.php
+++ update_faculty.php
@@ -1,5 +1,10 @@
+
 <?php
 include './database/config.php';
+session_start();
+if(!isset($_SESSION['username'])){
+ header("Location:login.php");
+}
 if(isset($_POST['faculty'])){
     $fid = $_POST['fid'];
  $faculty_name=mysqli_real_escape_string($conn,$_POST['faculty_name']);
@@ -8,6 +13,7 @@ if(isset($_POST['faculty'])){
  if(mysqli_query($conn,$sql)){
     header("location:faculty.php");
     echo "update data successfully! ";
+    exit();
  }
  else{
     echo "Update data failed";
@@ -27,29 +33,45 @@ if(isset($_POST['faculty'])){
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-    <?php 
+    <div class="container">
+      <div class="row">
+  
+        <div class="col-md-8 mx-auto">
+          <div class="card bg-light">
+            <div class="card-header bg-success text-dark text-center">
+              <b>Update faculty</b>
+            </div>
+            <?php 
     $fid =$_GET['fid'];
      $sqli = "SELECT * FROM faculty WHERE faculty_id='$fid'";
    $resuly = mysqli_query($conn,$sqli) or die("Query Failed");
-   if(mysqli_num_rows($resuly)){
+   if(mysqli_num_rows($resuly)>0){
     while($row = mysqli_fetch_array($resuly)){
    
 
     ?>
-  <h1>Faculty Update</h1>
-      <form action="" Method="POST" enctype="multipart/form-data">
-        <input type="hidden" name="fid" value="<?php echo $row['faculty_id'] ?>">
-        <div class="input-box">
-            <label for="">Faculty Name:</label>
-            <input type="text" name="faculty_name" value=<?php echo $row['faculty_name'] ?>>
+            <div class="card-body">
+              <form action="" method="POST" enctype="multipart/form-data">
+              <input type="hidden" name="fid" value="<?php echo $row['faculty_id'] ?>">
+                <div class="form-group">
+                <label for="">Faculty Name:</label>
+            <input type="text" name="faculty_name" value="<?php  echo $row['faculty_name']?>" class="form-control">
+                </div>
+                <div class="form-group">
+                <label for="">Designation:</label>
+            <input type="text" name="designation"  value="<?php echo $row['designation'] ?>" class="form-control">
+                </div>
+                <div class="form-group">
+                  <button type="submit" name="faculty" class="btn btn-primary text-dark">Update</button>
+                </div>
+              </form>
+              <?php  } }?>
+            </div>
+               
+          </div>
         </div>
-        <div class="input-box">
-            <label for="">Designation:</label>
-            <input type="text" name="designation" value="<?php echo $row['designation'] ?>">
-        </div>
-        <input type="submit" name="faculty" value="Update">
-      </form>
-      <?php  } }?>
+      </div>
+    </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
